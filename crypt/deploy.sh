0dbd377dbda4593c66646fcdace65b99798dbf2a
greysavior <greysavior@foxmail.com>
2017-01-03 23:58:19 +0800

    add deploy.sh for crypt, reorganize structure

---
+++ crypt/deploy.sh
@@ -0,0 +1,9 @@
+LOCAL_BIN=/usr/local/bin
+if [ ! -d "$LOCAL_BIN" ]; then
+    mkdir -p $LOCAL_BIN 
+fi
+cp *.sh $LOCAL_BIN
+if [[ "$PATH" == *"$LOCAL_BIN"* ]]; then
+    PATH=$PATH:$LOCAL_BIN
+fi
+chmod a+x $LOCAL_BIN/*.sh
