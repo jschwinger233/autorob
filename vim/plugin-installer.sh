abcbf7175d0fbc42795caee4391aaa6b2e91ad5b
greysavior <greysavior@foxmail.com>
2017-01-05 00:57:23 +0800

    add vim8 installation & python-dev requirement

--- vim/plugin-installer.sh
+++ vim/plugin-installer.sh
@@ -1,7 +1,14 @@
 # vim 7.4.143+ and CMake required
+# run with sudo
 
 # update .vimrc
 cp vimrc ~/.vimrc
+
+# upgrade vim8.0
+git clone https://github.com/vim/vim.git ~/vim
+# python-dev required
+(cd ~/vim && ./configure --with-features=huge --enable-pythoninterp=yes --enable-cscope --enable-fontset --enable-perlinterp --enable-rubyinterp --with-python-config-dir=/usr/lib/python2.7/config --prefix=/usr/local && make && make install)
+
 
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle 
@@ -12,4 +19,5 @@ git clone https://github.com/klen/python-mode.git ~/.vim/bundle/python-mode
 
 # install YouCompleteMe
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe/
-(cd ~/.vim/bundle/YouCompleteMe/ && git submodule update --init --recursive && python install.py)
+# python-dev required
+(cd ~/.vim/bundle/YouCompleteMe/ && git submodule update --init --recursive && sh install.py)
