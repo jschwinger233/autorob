fa915c0106de1d2bb8db76ec01584048d9d1b369
greysavior <greysavior@foxmail.com>
2017-01-12 16:11:27 +0000

    fix vim version defects

--- vim/plugin-installer.sh
+++ vim/plugin-installer.sh
@@ -6,9 +6,9 @@ set -x
 cp vimrc ~/.vimrc
 
 # upgrade vim8.0 if necessary (>7.4.143)
-VIM_VERSION_BIG=$(vim --version | grep '(?<=Vi IMproved )\d+.\d+' -Po)
-VIM_VERSION_SMALL=$(vim --version | grep '(?<=Included patches: )\d+-\d+' -Po)
-if ! ([ ${VIM_VERSION_BIG%.*} -gt 7 ] || [ ${VIM_VERSION_BIG#*.} -gt 4 ] || [ $VIM_VERSION_SMALL -gt 143 ]); then
+VIM_VERSION_BIG=$(vim --version | sed -n '1p' | grep '\d+\.\d' -Po)
+VIM_VERSION_SMALL=$(vim --version | sed -n '2p' | grep '\d+-\d+' -Po)
+if ! ([ ${VIM_VERSION_BIG%.*} -gt 7 ] || [ ${VIM_VERSION_BIG#*.} -gt 4 ] || [ ${VIM_VERSION_SMALL#*-} -gt 143 ]); then
     git clone https://github.com/vim/vim.git ~/vim
     # python-dev required
     (cd ~/vim && ./configure --with-features=huge --enable-pythoninterp=yes --enable-cscope --enable-fontset --enable-perlinterp --enable-rubyinterp --with-python-config-dir=/usr/lib/python2.7/config --prefix=/usr/local && make && make install)
