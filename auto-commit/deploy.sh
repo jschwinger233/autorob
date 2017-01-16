ddcddb6b43954a35801a4f0d61a8b1c02968b855
jschwinger23j <greysavior@qq.com>
2017-01-16 11:17:10 +0800

    fix crontab deploy

--- auto-commit/deploy.sh
+++ auto-commit/deploy.sh
@@ -3,3 +3,7 @@ set -x
 CUR_DIR=$(cd $(dirname $0); pwd)
 cp $CUR_DIR/auto-commit.sh /etc/cron.daily/
 chmod a+x /etc/cron.daily/auto-commit.sh
+
+cat <<EOF >> /var/spool/cron/root
+30 23 * * * /bin/bash $CUR_DIR/auto-commit.sh
+EOF
