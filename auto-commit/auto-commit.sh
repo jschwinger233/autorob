002d15609c4ddb7471ac074aea618dd971c4bc7d
jschwinger23 <greysavior@qq.com>
2017-01-20 00:01:18 +0800

    add sudo for vim-plugin-installer

--- auto-commit/auto-commit.sh
+++ auto-commit/auto-commit.sh
@@ -2,6 +2,11 @@
 # Auto-commit for 2017 Plan
 
 ME=jschwinger23
+TODAY=$(date +%Y-%m-%d)
+NOW=$(date +%Y-%m-%dT%H:%M:%S)
+CUR_DIR=$(cd $(dirname $0); pwd)
+
+exec 3>>$CUR_DIR/append
 
 MY_GITHUB=$(curl "https://github.com/$ME" --connect-timeout 3)
 while [ "$?" != "0" ]; do
@@ -12,16 +17,12 @@ if [ "$DEBUG" == "1" ]; then
     echo $MY_GITHUB > github.ignore
 fi
 
-TODAY=$(date +%Y-%m-%d)
 echo $MY_GITHUB | grep -Pq 'data-count="(?!0)[^"]+" data-date="'$TODAY
 if [ $? -eq 0 ]; then
-    echo 'pushed already, congrats!'
+    echo $NOW':pushed already, congrats!' >>&3
     exit 0
 fi
 
-echo 'busy day, push now!'
-CUR_DIR=$(cd $(dirname $0); pwd)
-
-NOW=$(date +%Y-%m-%dT%H:%M:%S)
-echo $NOW >> $CUR_DIR/append
+echo $NOW':busy day, push now!' > &3
 cd $CUR_DIR && git commit -am'auto-commit'$NOW && git push origin master
+exec 3>&-
