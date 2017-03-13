8912174df96223587837a9e88c4fbfca30845a93
greysavior <greysavior@foxmail.com>
2017-03-13 23:30:54 +0800

    auto-commit2017-03-13T23:30:01

--- mysql-backup/mysql-backup.sh
+++ mysql-backup/mysql-backup.sh
@@ -1,5 +1,5 @@
 if [[ -z "$USER" ]] | [[ -z "$PASSWD" ]] | [[ -z "$DB" ]] | [[ -z "$TB" ]] | [[ -z "$CRY" ]]; then
-    echo "env USER / PASSWD / DB / TB shall be set properly!"
+    echo "env USER / PASSWD / DB / TB / CRY shall be set properly!"
     exit
 fi
 TODAY=$(date +%Y-%m-%d)
@@ -7,4 +7,4 @@ CUR_DIR=$(cd $(dirname $0); pwd)
 BAK="$CUR_DIR/$DB.$TB"
 mysqldump -u"$USER" -p"$PASSWD" "$DB" "$TB" > "$BAK"
 crypt.sh "$BAK" "$CRY" && rm -f $BAK
-git add "$BAK.gpg" && git commit -m "backup MySQL $DB.$TB at $TODAY" && git push 
+cd $CUR_DIR && git add "$BAK.gpg" && git commit -m "backup MySQL $DB.$TB at $TODAY" && git push 
